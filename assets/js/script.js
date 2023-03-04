27dcc2bf99dc2737ca2573fabe7ec326e51a20ba
Rafael Sanzio <126710574+SanzioRaphael@users.noreply.github.com>
2023-03-03 21:11:05 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,8 @@
-
+/*Selected Options*/
+$(document).ready(function() {
+  $('#options, #options2, #option3, #option4').change(function() {
+    var selectedClass = $(this).val();
+    $('#animatio-class-view').text(selectedClass);
+    $('#animatio-text').removeClass().addClass(selectedClass);
+  });
+});
